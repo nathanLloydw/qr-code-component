3362c7ce1e475315e3b3d9e041eff8844075de8a
nlloydw <nathan@revolutiongrowth.co.uk>
2022-07-11 10:18:24 +0100

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,26 +5,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-        colors:{
-            'violet':
-            {
-                DEFAULT:'hsl(257, 40%, 49%)'
-            },
-            'magenta':
-            {
-                DEFAULT: 'hsl(300, 69%, 71%)'
-            }
-        },
-        fontFamily:
-            {
-                'poppins':['Poppins'],
-                'open-sans':['Open Sans']
-            },
-        fontSize:
-            {
-                '4.5xl':'2.5rem'
-            }
-
     },
   },
   plugins: [],
